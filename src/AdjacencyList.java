97d6e18d37a382db4910994a0b32be2e12543de7
Dustin Cassell <dustin.cassell@live.com>
2022-04-05 23:31:14 -0700

    label test passed

--- src/AdjacencyList.java
+++ src/AdjacencyList.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * File: AdjacencyList.java
  * Date: Spring 2022
  * Auth: Dustin Cassell
@@ -6,13 +6,13 @@
  */
 
 
-/* TODO: 
+/* TODO:
  *   (1) Implement the functions below. Note some are "stubbed" out so
  *       that the code will compile. You will need to implement all of
  *       the functions (even those stubbed out).
  *   (2) Make sure your code is commented and well formatted
  *   (3) Fill out the file header above
- */ 
+ */
 
 
 import java.util.ArrayList;
@@ -25,123 +25,144 @@ import java.util.Set;
 
 public class AdjacencyList<T> implements Graph<T> {
 
-  // the total number of nodes in the graph (numbered 0 to nodes-1)
-  private int nodeCount;
+    // the total number of nodes in the graph (numbered 0 to nodes-1)
+    private int nodeCount;
 
-  // the total number of edges in the graph 
-  private int edgeCount;
+    // the total number of edges in the graph
+    private int edgeCount;
 
-  // true if the graph is directed, false if undirected
-  private boolean directed;
+    // true if the graph is directed, false if undirected
+    private boolean directed;
 
-  // the adjacency list as an array of node-to-label mappings
-  private Map<Integer,T>[] adjList;
-
-
-  //--------------------------------------------------------------------
-  // constructor
-  //--------------------------------------------------------------------
-
-  // Initializes the graph with the given number of nodes and the
-  // given directionality.
-  public AdjacencyList(int nodeCount, boolean directed) {
-    this.nodeCount = nodeCount;
-    this.directed = directed;
-    adjList = new HashMap[nodeCount];
-    // initialize each array element to an empty hashmap
-    for (int i = 0; i < nodeCount; ++i)
-      adjList[i] = new HashMap<>();
-  }
+    // the adjacency list as an array of node-to-label mappings
+    private Map<Integer,T>[] adjList;
 
 
-  //--------------------------------------------------------------------
-  // graph methods
-  //--------------------------------------------------------------------
+    //--------------------------------------------------------------------
+    // constructor
+    //--------------------------------------------------------------------
 
-  // Returns true if the graph is directed and false if undirected.
+    // Initializes the graph with the given number of nodes and the
+    // given directionality.
+    public AdjacencyList(int nodeCount, boolean directed) {
+        this.nodeCount = nodeCount;
+        this.directed = directed;
+        adjList = new HashMap[nodeCount];
+        // initialize each array element to an empty hashmap
+        for (int i = 0; i < nodeCount; ++i)
+            adjList[i] = new HashMap<>();
+    }
 
-  /**
-   * Purpose: Generic getter for the field boolean directed.
-   * @return true if the graph is directed, otherwise false.
-   */
-  public boolean directed() {
-    return directed;
-  }
 
-  // Adds a labeled edge to the graph from node1 to node2. Only adds
-  // the edge if valid nodes are given and the edge doesn't already
-  // exist.
-  public void add(int node1, T label, int node2) {
-    // TODO: finish this method
-  }
+    //--------------------------------------------------------------------
+    // graph methods
+    //--------------------------------------------------------------------
 
-  // Removes an edge from the graph if it exists
-  public void remove(int node1, int node2) {
-    // TODO: finish this method
-  }
+    // Returns true if the graph is directed and false if undirected.
 
-  // Updates the label for a given edge. If the edge does not exist,
-  // the edge is not added.
-  public void set(int node1, T label, int node2) {
-    // TODO: finish this method
-  }
+    /**
+     * Purpose: Generic getter for the field boolean directed.
+     * @return true if the graph is directed, otherwise false.
+     */
+    public boolean directed() {
+        return directed;
+    }
 
-  // Returns the label on the give edge.
-  // Pre: the edge exists
-  public T label(int node1, int node2) {
-    // TODO: finish this method
-    return null;
-  }
+    // Adds a labeled edge to the graph from node1 to node2. Only adds
+    // the edge if valid nodes are given and the edge doesn't already
+    // exist.
+    public void add(int node1, T label, int node2) {
+        // TODO: finish this method
+        if (directed) {
+            // check if edge already exists assuming the start is node1 for directed
+            if (adjList[node1].isEmpty()) {
+                // if the nodes are valid nodes
+                adjList[node1].put(node2, label);
+            }
+        } else {
+            // check if edge already exists
+            if (adjList[node1].isEmpty() && adjList[node2].isEmpty()) {
+                adjList[node1].put(node2, label);
+                adjList[node2].put(node1, label);
+            }
+        }
+    }
 
-  // Returns true if the graph contains an edge from node1 to node2
-  // and false otherwise. Note that if the graph is undirected, no
-  // directionality is implied by the edge.
-  public boolean hasEdge(int node1, int node2) {
-    // TODO: finish this method
-    return false;
-  }
+    // Removes an edge from the graph if it exists
+    public void remove(int node1, int node2) {
+        // TODO: finish this method
+    }
 
-  // Returns all nodes adjacent to the given node. In a directed
-  // graph, this includes the set of nodes on outgoing and incoming
-  // edges, i.e., that lie on out-edges and in-edges. 
-  public List<Integer> adjacent(int node) {
-    // TODO: finish this method
-    return null;
-  }
+    // Updates the label for a given edge. If the edge does not exist,
+    // the edge is not added.
+    public void set(int node1, T label, int node2) {
+        // TODO: finish this method
+        if (directed) {
+            if (!adjList[node1].isEmpty() && !adjList[node2].isEmpty()) {
+                // if node 1 and 2 exists, the label will be changed
+                adjList[node1].put(node2, label);
+                adjList[node2].put(node1, label);
+            }
+        } else {
 
-  // Returns all nodes on an outgoing edge (out-edge) from the given
-  // node. For an undirected graph, this method returns the same nodes
-  // as adjacent.
-  public List<Integer> outNodes(int node) {
-    // TODO: finish this method
-    return null;
-  }
+        }
+    }
 
-  // Returns all nodes on an incoming edge (in-edge) from the given
-  // node. For an undirected graph, this method returns the same nodes
-  // as adjacent. 
-  public List<Integer> inNodes(int node) {
-    // TODO: finish this method
-    return null;
-  }
+    // Returns the label on the give edge.
+    // Pre: the edge exists
+    public T label(int node1, int node2) {
+        return adjList[node1].get(node2);
+    }
 
-  /**
-   * Purpose: Generic getter for the number of nodes currently in the graph.
-   * @return Integer count of nodes in Graph.
-   */
-  public int nodeCount() {
-    return nodeCount;
-  }
+    // Returns true if the graph contains an edge from node1 to node2
+    // and false otherwise. Note that if the graph is undirected, no
+    // directionality is implied by the edge.
+    public boolean hasEdge(int node1, int node2) {
+        // TODO: finish this method
+        return false;
+    }
 
-  /**
-   * Purpose: Generic getter for the number of edges currently in the graph.
-   * @return Integer count of edges in Graph.
-   *    NOTE: In a directed graph, it returns the total number of directed
-   *          edges. In an undirected graph, it returns the number of
-   *          undirected edges.
-   */
-  public int edgeCount() {
-    return edgeCount;
-  }
+    // Returns all nodes adjacent to the given node. In a directed
+    // graph, this includes the set of nodes on outgoing and incoming
+    // edges, i.e., that lie on out-edges and in-edges.
+    public List<Integer> adjacent(int node) {
+        // TODO: finish this method
+        return null;
+    }
+
+    // Returns all nodes on an outgoing edge (out-edge) from the given
+    // node. For an undirected graph, this method returns the same nodes
+    // as adjacent.
+    public List<Integer> outNodes(int node) {
+        // TODO: finish this method
+        return null;
+    }
+
+    // Returns all nodes on an incoming edge (in-edge) from the given
+    // node. For an undirected graph, this method returns the same nodes
+    // as adjacent.
+    public List<Integer> inNodes(int node) {
+        // TODO: finish this method
+        return null;
+    }
+
+    /**
+     * Purpose: Generic getter for the number of nodes currently in the graph.
+     * @return Integer count of nodes in Graph.
+     */
+    public int nodeCount() {
+        return nodeCount;
+    }
+
+    /**
+     * Purpose: Generic getter for the number of edges currently in the graph.
+     * @return Integer count of edges in Graph.
+     *    NOTE: In a directed graph, it returns the total number of directed
+     *          edges. In an undirected graph, it returns the number of
+     *          undirected edges.
+     */
+    public int edgeCount() {
+        return edgeCount;
+    }
 
 }
